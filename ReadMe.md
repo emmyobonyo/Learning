d0cd668c61db2de907a901be18aa1a15162532bb
Emmanuel obonyo <emmyobonyo@gmail.com>
2023-11-29 11:17:13 +0300

    create node server with http

---
+++ ReadMe.md
@@ -0,0 +1,6 @@
+Posting Process.
+
+1. Push to Github.
+2. Post to Medium.
+3. Post to LinkedIn.
+4. Post to Dev.io.
---
+++ create-node-server/index.js
@@ -0,0 +1,29 @@
+// HTTP is an inbuilt model that comes bundled with node.
+const http = require('http');
+
+// Initialise a server called server
+const server = http.createServer((req, res) => {
+  // req is listening to data coming in from other computers, the clients. It's siting there waiting to here what the other computers want to tell it.For example we get the client URL using req.url
+
+  // res is the server response. It's responding to a request (req) made ny another computer. We can add special messages called headers to our response to help the other computer understand our message better.
+
+  // We can write these extra messages using the writeHead method which takes a status code and a content-type among other parameters.
+
+  res.writeHead(200, {
+    // The content-type of our response. Basically it's telling the computer what kind of message is in the response. In this case it's going to be JSON.
+    'Contemt-Type': 'application/json'
+  });
+
+  // Lets end our response. We use res.end() to tell the other computer that our response is done.
+  // res.end function expects a string so we need to chain it with a JSON.stringify() function to change our JSON response into a string. Not very complicated.
+
+  res.end(JSON.stringify({
+    id: 1,
+    name: 'Emmanuel Obonyo',
+  }))
+})
+
+// Start your server. Without this your server will not start. It's like opening shop for business.
+server.listen(3000, () => {
+  console.log('server listening on 3000')
+});
