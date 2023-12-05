0f86b57fbac0136da151b4621e99b192d40c3938
Emmanuel obonyo <emmyobonyo@gmail.com>
2023-12-05 11:11:58 +0300

    add a user to the database

--- passport-ninja/services/passport-setup.js
+++ passport-ninja/services/passport-setup.js
@@ -1,6 +1,30 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
+const User = require('../models/user-model');
+
+// An alternate longer way of doing things
+// function assignUser(profile) {
+//   const user = new User({
+//     username: profile.displayName,
+//     googleid: profile.id,
+//   });
+
+//   return user;
+// }
+
+// async function saveUser(profile) {
+//   await User.findOneAndUpdate(
+//     {
+//       googleid: profile.id,
+//     },
+//     assignUser(profile),
+//     {
+//       upsert: true,
+//     }
+//   );
+//   console.log('User has been saved');
+// }
 
 passport.use(
   new GoogleStrategy(
@@ -11,8 +35,25 @@ passport.use(
     },
     (accessToken, refreshToken, profile, done) => {
       // passport callback function
-      console.log('pasport callback function fired');
-      console.log(profile);
+      /* console.log('pasport callback function fired');
+      console.log(profile); */
+
+      // Check if User already exists in the database.
+      User.findOne({ googleid: profile.id }).then((currentUser) => {
+        if (currentUser) {
+          // Already have a User
+          console.log('Current User is', currentUser);
+        } else {
+          new User({
+            googleid: profile.googleid,
+            username: profile.displayName,
+          })
+            .save()
+            .then((newUser) => {
+              console.log('New User', newUser);
+            });
+        }
+      });
     }
   )
 );
