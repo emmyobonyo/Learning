e3ccd340920676896f603f486f805e0114b882bd
Emmanuel obonyo <emmyobonyo@gmail.com>
2023-12-06 14:59:00 +0300

    connect Mongo DB

--- passport-github-authentication/app.js
+++ passport-github-authentication/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const authRoutes = require('./routes/auth');
+const mongoose = require('mongoose');
 
 const app = express();
 
 const PORT = 3000;
+
+const MONGO_URL =
+  'mongodb+srv://komagumobonyo:G89uxVxOPI6Ag3u2@cluster0.p9gn28p.mongodb.net/learn';
+
+async function mongoConnect() {
+  await mongoose.connect(MONGO_URL);
+  console.log('Connection to MongoDb done');
+}
 
 // Set View engine
 app.set('view engine', 'ejs');
@@ -16,6 +25,11 @@ app.use('/', (req, res) => {
   res.render('home');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+async function startServer() {
+  await mongoConnect();
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+startServer();
