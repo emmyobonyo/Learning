bd0cd87abd667589907d1fbafce41df38c11cd90
Emmanuel obonyo <emmyobonyo@gmail.com>
2023-12-12 09:37:31 +0300

    add some auth routes

--- passport-github-auth/app.js
+++ passport-github-auth/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
 // Creare home route
+
+app.use('/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.render('home');
--- passport-github-auth/routes/auth.js
+++ passport-github-auth/routes/auth.js
@@ -1,13 +1,15 @@
-const router = require('express').router();
+const router = require('express').Router();
 
 router.get('/logout', (req, res) => {
   res.send('this is the logout route');
 });
 
 router.get('/login', (req, res) => {
-  res.render('/login');
+  res.render('login');
 });
 
 router.get('/github', (req, res) => {
   res.send('This is our github route');
 });
+
+module.exports = router;
