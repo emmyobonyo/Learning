e4cc4a114c992df626c3fa1f747aabbc731cd90a
Emmanuel obonyo <emmyobonyo@gmail.com>
2023-12-13 11:21:28 +0300

    dont allow existing users to the db

--- passport-github-auth/app.js
+++ passport-github-auth/app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const authRoutes = require('./routes/auth');
 const passportSetup = require('./services/passport-setup');
+const mongoose = require('mongoose');
+
+const MONGO_URL =
+  'mongodb+srv://komagumobonyo:G89uxVxOPI6Ag3u2@cluster0.p9gn28p.mongodb.net/learn';
+
+async function mongoConnect() {
+  await mongoose.connect(MONGO_URL);
+  console.log('Mongoose connection sooo ready');
+}
 
 const app = express();
 
@@ -14,6 +23,11 @@ app.get('/', (req, res) => {
   res.render('home');
 });
 
-app.listen(3000, () => {
-  console.log(`Listening on port 3000`);
-});
+async function startServer() {
+  await mongoConnect();
+  app.listen(3000, () => {
+    console.log(`Listening on port 3000`);
+  });
+}
+
+startServer();
--- passport-github-auth/services/passport-setup.js
+++ passport-github-auth/services/passport-setup.js
@@ -1,6 +1,7 @@
 const GithubStrategy = require('passport-github2');
 const passport = require('passport');
 const keys = require('./keys');
+const User = require('../models/user');
 
 passport.use(
   new GithubStrategy(
@@ -10,9 +11,21 @@ passport.use(
       clientSecret: keys.github.clientSecret,
     },
     (accessToken, refreshToken, profile, done) => {
-      //passport cllback function
-      console.log('passport fired');
-      console.log(profile);
+      User.findOne({ id: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          console.log('User already exists in the database', existingUser);
+        } else {
+          new User({
+            id: profile.id,
+            username: profile.username,
+            name: profile.displayName,
+          })
+            .save()
+            .then((currentUser) => {
+              console.log(`Current User ${currentUser}`);
+            });
+        }
+      });
     }
   )
 );
